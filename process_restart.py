7b5f19da1f73d3d0b4f52b4300c13c7dcc56482a
misfits42 <manuelshelton@gmail.com>
2019-05-30 01:38:51 +0200

    first version of generalization. Todo the except path not working correctly.

--- process_restart.py
+++ process_restart.py
@@ -1,34 +1,49 @@
 #! python3
-import psutil,time,subprocess,logging
+import logging
+import pathlib
+import psutil
+import subprocess
+import time
 
 logging.basicConfig(filename='processRestart.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # to disable logging uncomment next line
 # logging.disable(logging.CRITICAL)
 # TODO: rewrite so that this works cross platform and takes process name, time, sleep, debugging as arguments
-def process_mon_restart():
+def process_mon_restart(proc: str, chk_time: int = 300, startup_time: int = 30):
     '''
     This function monitors a process and if it stops for some reason, restarts it again.
-    currently this function takes no arguments.
+    :proc: process name to check
+    :chk_time: time in seconds to wait until next check
+    :startup_time: time in seconds to wait after process startup until next check is performed
+
     :return: no return value
     '''
     logging.info('Start')
     # run 4-ever
     while True:
-        # check if outlook is still running
-        while ('OUTLOOK.EXE' in (p.name() for p in psutil.process_iter())):
-            # check every 10 minutes if outlook is still running.
-            time.sleep(10)
-        subprocess.run(r'"C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"')
-        logging.info('Outlook EXE has been restarted')
-        # older way:
-        # os.system(r'"C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE"')
-        # give outlook time to start up
-        time.sleep(30)
+        # check if proc running
+        proc_info = [p.info for p in psutil.process_iter(attrs=['name', 'exe']) if proc in p.info['name']]
+        try:
+            while (proc_info[0]["name"] in (p.name() for p in psutil.process_iter())):
+                time.sleep(chk_time)
+            # example: proc_info[0]
+            # {'exe': 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', 'name': 'chrome.exe'}
+            # proc_info[0]['exe']
+            # 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+        except IndexError:
+            logging.info(f'{proc}: is not running on startup')
+            proc = str(input('Process you entered does not exist, please enter a valid process name:'))
+            continue
+        subprocess.run(pathlib.Path(proc_info[0]['exe'])) # uses pathlib.Path which makes it OS independant
+        logging.info(f'{proc_info[0]["name"]} has been restarted')
+        # give process time to start up
+        time.sleep(startup_time)
 
 
 if __name__ == '__main__':
-    process_mon_restart()
+    process = str(input('Please enter process to monitor and automatic restart:'))
+    process_mon_restart(process)
 
 
 
