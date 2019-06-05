35f5591248a8309424eb70a64b2de2c347ce4482
misfit42 <16911012+misfit42@users.noreply.github.com>
2019-06-05 13:48:20 +0200

    generalized and added logging for startup return code

--- process_restart.py
+++ process_restart.py
@@ -5,15 +5,8 @@ import psutil
 import subprocess
 import time
 
-logging.basicConfig(filename='processRestart.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-# to disable logging uncomment next line
-# logging.disable(logging.CRITICAL)
-
-
-def process_mon_restart(proc: str, chk_time: int = 300, startup_time: int = 30):
+def process_restart(proc: str, chk_time: int = 300, startup_time: int = 30):
     """
     This function monitors a running process and if it stops for some reason, restarts it again.
     This actions are logged in processRestart.log in same directory.#TODO make file path a input parameter for function
@@ -23,29 +16,29 @@ def process_mon_restart(proc: str, chk_time: int = 300, startup_time: int = 30):
     #TODO handle error
     :return: Error if entered process is not running on start of process_mon_restart
     """
+    logging.basicConfig(filename='processRestart.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+    # to disable logging uncomment next line
+    # logging.disable(logging.CRITICAL)
 
     logging.info('Start')
 
     # run 4-ever
     while True:
         # check if process running, use casefold to compensate for mixed case executable names.
-        # Save as list to preserve info for later useage.
+        # Save as list to preserve information for later useage.
         proc_info = [ p.info for p in psutil.process_iter(attrs=[ 'name', 'exe' ]) if
                       proc.casefold() in p.info[ 'name' ].casefold() ]
         logging.info(f'Content of procInfo:  {proc_info}; '
                      f'this should contain the entered executable')
         while (proc_info[ 0 ][ "name" ] in (p.name() for p in psutil.process_iter())):
             time.sleep(chk_time)
-        # example: proc_info[0]
-        # {'exe': 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', 'name': 'chrome.exe'}
-        # proc_info[0]['exe']
-        # 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
 
         logging.info(f'{proc_info[ 0 ][ "exe" ]} will try to be restarted')
         # uses pathlib.Path which makes it OS independent object.
         # pass it to str() to pass the string of the path and not the pathlib.Path object.
-        subprocess.run(str(pathlib.Path(proc_info[ 0 ][ 'exe' ])))
-        logging.info(f'{proc_info[ 0 ][ "name" ]} has been restarted successfully')
+        proc_started = subprocess.run(str(pathlib.Path(proc_info[ 0 ][ 'exe' ])))
+        logging.info(f'{proc_info[ 0 ][ "name" ]} has been restarted: {proc_started.args}, {proc_started.returncode}')
         # give process time to start up
         time.sleep(startup_time)
 
